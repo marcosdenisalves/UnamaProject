9d1ae86f0be5f970ad3dd7f095f4142e2a5de353
Study Profile <dee.alves2@gmail.com>
2021-03-17 20:37:30 -0300

    Adicionando busca porId

--- src/main/java/br/com/unamaproject/server/resources/LaboratorioResource.java
+++ src/main/java/br/com/unamaproject/server/resources/LaboratorioResource.java
@@ -1,15 +1,25 @@
 package br.com.unamaproject.server.resources;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.unamaproject.server.domain.Laboratorio;
+import br.com.unamaproject.server.services.LaboratorioService;
 
 @RestController
 @RequestMapping(value = "/laboratorios")
 public class LaboratorioResource {
 
-	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "Funcionando!";
+	@Autowired
+	private LaboratorioService service;
+	
+	@RequestMapping(value="/{id}", method = RequestMethod.GET)
+	public ResponseEntity<Laboratorio> listar(@PathVariable Integer id) {
+		Laboratorio obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 }
