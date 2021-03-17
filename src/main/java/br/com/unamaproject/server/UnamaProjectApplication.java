86c1c3a5dcfac8b924f06d8fc73e579ae2674ee3
Study Profile <dee.alves2@gmail.com>
2021-03-17 19:24:10 -0300

    Criando Projeto

---
+++ src/main/java/br/com/unamaproject/server/UnamaProjectApplication.java
@@ -0,0 +1,13 @@
+package br.com.unamaproject.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnamaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnamaProjectApplication.class, args);
+	}
+
+}
