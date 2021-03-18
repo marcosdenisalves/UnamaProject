5a577806444f8fc6db85686cab2cb2ed447b1597
Study Profile <dee.alves2@gmail.com>
2021-03-17 21:38:33 -0300

    Usuario Resource

---
+++ src/main/java/br/com/unamaproject/server/resources/UsuarioResource.java
@@ -0,0 +1,25 @@
+package br.com.unamaproject.server.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.unamaproject.server.domain.Usuario;
+import br.com.unamaproject.server.services.UsuarioService;
+
+@RestController
+@RequestMapping(value = "/laboratorios")
+public class UsuarioResource {
+
+	@Autowired
+	private UsuarioService service;
+	
+	@RequestMapping(value="/{id}", method = RequestMethod.GET)
+	public ResponseEntity<Usuario> listar(@PathVariable Integer id) {
+		Usuario obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+}
