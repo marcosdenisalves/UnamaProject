06a6b05c959db6bc921365200e37fc6b18fa9bb6
Study Profile <dee.alves2@gmail.com>
2021-03-20 20:37:00 -0300

    Adicionando PostMethod

--- src/main/java/br/com/unamaproject/server/resources/UsuarioResource.java
+++ src/main/java/br/com/unamaproject/server/resources/UsuarioResource.java
@@ -1,11 +1,15 @@
 package br.com.unamaproject.server.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.unamaproject.server.domain.Usuario;
 import br.com.unamaproject.server.services.UsuarioService;
@@ -21,5 +25,14 @@ public class UsuarioResource {
 	public ResponseEntity<Usuario> listar(@PathVariable Integer id) {
 		Usuario obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Usuario obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder
+				.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 }
