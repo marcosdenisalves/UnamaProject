56e30210613b34abf90872510b53a9fe885e41f7
Study Profile <dee.alves2@gmail.com>
2021-03-20 21:30:53 -0300

    Deletando Usuario

--- src/main/java/br/com/unamaproject/server/services/UsuarioService.java
+++ src/main/java/br/com/unamaproject/server/services/UsuarioService.java
@@ -5,7 +5,9 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.unamaproject.server.domain.Avaliacao;
 import br.com.unamaproject.server.domain.Usuario;
+import br.com.unamaproject.server.repositories.AvaliacaoRepository;
 import br.com.unamaproject.server.repositories.UsuarioRepository;
 import br.com.unamaproject.server.service.exceptions.ObjectNotFoundException;
 
@@ -13,21 +15,34 @@ import br.com.unamaproject.server.service.exceptions.ObjectNotFoundException;
 public class UsuarioService {
 
 	@Autowired
-	private UsuarioRepository repository;
+	private UsuarioRepository usuarioRepository;
+	
+	@Autowired
+	private AvaliacaoRepository avaliacaoRepository;
 	
 	public Usuario findById(Integer id) {
-		Optional<Usuario> obj = repository.findById(id);
+		Optional<Usuario> obj = usuarioRepository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				 "Objeto não encontrado! Id: " + id + ", Tipo: " + Usuario.class.getName()));
 	}
 
 	public Usuario insert(Usuario usuario) {
 		usuario.setId(null);
-		return repository.save(usuario);
+		return usuarioRepository.save(usuario);
 	}
 
 	public Usuario update(Usuario obj) {
 		findById(obj.getId());
-		return repository.save(obj);
+		return usuarioRepository.save(obj);
+	}
+
+	public void delete(Integer id) {
+		Usuario usuario = findById(id);
+		if (!usuario.getAvaliacoes().isEmpty())
+			for (Avaliacao avaliacao : usuario.getAvaliacoes()) {
+				avaliacaoRepository.deleteById(avaliacao.getId());
+			}
+
+		usuarioRepository.deleteById(id);
 	}
 } 
