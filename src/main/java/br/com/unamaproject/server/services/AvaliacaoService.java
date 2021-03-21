2be9dbfdca02b7432e5bbc2dc6c3d7573a7bc8d9
Study Profile <dee.alves2@gmail.com>
2021-03-21 20:33:23 -0300

    Adicionando métodos crud para AvaliacaoService

--- src/main/java/br/com/unamaproject/server/services/AvaliacaoService.java
+++ src/main/java/br/com/unamaproject/server/services/AvaliacaoService.java
@@ -1,12 +1,17 @@
 package br.com.unamaproject.server.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import br.com.unamaproject.server.domain.Avaliacao;
 import br.com.unamaproject.server.repositories.AvaliacaoRepository;
+import br.com.unamaproject.server.service.exceptions.DataIntegrityException;
 import br.com.unamaproject.server.service.exceptions.ObjectNotFoundException;
 
 @Service
@@ -19,5 +24,35 @@ public class AvaliacaoService {
 		Optional<Avaliacao> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				 "Objeto não encontrado! Id: " + id + ", Tipo: " + Avaliacao.class.getName()));
+	}
+
+	public Avaliacao insert(Avaliacao obj) {
+		obj.setId(null);
+		return repository.save(obj);
+	}
+
+	public Avaliacao update(Avaliacao obj) {
+		findById(obj.getId());
+		return repository.save(obj);
+	}
+
+	public void delete(Integer id) {
+		findById(id);
+		try {
+			repository.deleteById(id);
+		}catch (DataIntegrityException e) {
+			throw new DataIntegrityException
+				("Não foi possivel remover a " + Avaliacao.class + 
+						", a entidade contem usuários atrelados a ela.");
+		}
+	}
+
+	public List<Avaliacao> findAll() {
+		return repository.findAll();
+	}
+
+	public Page<Avaliacao> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage,Direction.valueOf(direction), orderBy);
+		return repository.findAll(pageRequest);
 	}
 } 
