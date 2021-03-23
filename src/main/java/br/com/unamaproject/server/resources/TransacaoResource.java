0e49c19f648fd2c8fe95bd6bef88251fee41484c
Study Profile <dee.alves2@gmail.com>
2021-03-23 19:55:39 -0300

    Criando CrudMethods para Transacao

--- src/main/java/br/com/unamaproject/server/resources/TransacaoResource.java
+++ src/main/java/br/com/unamaproject/server/resources/TransacaoResource.java
@@ -1,11 +1,18 @@
 package br.com.unamaproject.server.resources;
 
+import java.net.URI;
+import java.util.List;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.unamaproject.server.domain.Transacao;
 import br.com.unamaproject.server.services.TransacaoService;
@@ -21,5 +28,33 @@ public class TransacaoResource {
 	public ResponseEntity<Transacao> listar(@PathVariable Integer id) {
 		Transacao obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody Transacao obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder
+				.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Void> update(@Valid @RequestBody Transacao obj, @PathVariable Integer id) {
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<Transacao>> findAll() {
+		List<Transacao> list = service.findAll();
+		return ResponseEntity.ok().body(list);
 	}
 }
--- src/main/java/br/com/unamaproject/server/services/TransacaoService.java
+++ src/main/java/br/com/unamaproject/server/services/TransacaoService.java
@@ -1,9 +1,14 @@
 package br.com.unamaproject.server.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import br.com.unamaproject.server.domain.Transacao;
 import br.com.unamaproject.server.repositories.TransacaoRepository;
@@ -19,5 +24,30 @@ public class TransacaoService {
 		Optional<Transacao> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				 "Objeto não encontrado! Id: " + id + ", Tipo: " + Transacao.class.getName()));
+	}
+
+	@Transactional
+	public Transacao insert(Transacao obj) {
+		obj.setId(null);
+		return repository.save(obj);
+	}
+
+	public Transacao update(Transacao obj) {
+		findById(obj.getId());
+		return repository.save(obj);
+	}
+
+	public void delete(Integer id) {
+		findById(id);
+		repository.deleteById(id);
+	}
+
+	public List<Transacao> findAll() {
+		return repository.findAll();
+	}
+
+	public Page<Transacao> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage,Direction.valueOf(direction), orderBy);
+		return repository.findAll(pageRequest);
 	}
 } 
