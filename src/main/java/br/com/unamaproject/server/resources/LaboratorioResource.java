958b71d2b858db10459af2685602b96799a5802d
Study Profile <dee.alves2@gmail.com>
2021-03-23 19:55:10 -0300

    Criando CrudMethods para laboratorio

--- src/main/java/br/com/unamaproject/server/resources/LaboratorioResource.java
+++ src/main/java/br/com/unamaproject/server/resources/LaboratorioResource.java
@@ -1,11 +1,20 @@
 package br.com.unamaproject.server.resources;
 
+import java.net.URI;
+import java.util.List;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.unamaproject.server.domain.Laboratorio;
 import br.com.unamaproject.server.services.LaboratorioService;
@@ -21,5 +30,44 @@ public class LaboratorioResource {
 	public ResponseEntity<Laboratorio> listar(@PathVariable Integer id) {
 		Laboratorio obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody Laboratorio obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder
+				.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Void> update(@Valid @RequestBody Laboratorio obj, @PathVariable Integer id) {
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<Laboratorio>> findAll() {
+		List<Laboratorio> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@RequestMapping(value = "/page", method = RequestMethod.GET)
+	public ResponseEntity<Page<Laboratorio>> findPage(
+			@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "24")	Integer linesPerPage,
+			@RequestParam(value = "orderBy", defaultValue = "nome") String orderBy,
+			@RequestParam(value = "direction", defaultValue = "ASC")	String direction) {
+		
+		Page<Laboratorio> list = service.findPage(page, linesPerPage, orderBy, direction);
+		return ResponseEntity.ok().body(list);
 	}
 }
--- src/main/java/br/com/unamaproject/server/services/LaboratorioService.java
+++ src/main/java/br/com/unamaproject/server/services/LaboratorioService.java
@@ -1,9 +1,14 @@
 package br.com.unamaproject.server.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import br.com.unamaproject.server.domain.Laboratorio;
 import br.com.unamaproject.server.repositories.LaboratorioRepository;
@@ -20,4 +25,28 @@ public class LaboratorioService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				 "Objeto não encontrado! Id: " + id + ", Tipo: " + Laboratorio.class.getName()));
 	}
-} 
+
+	@Transactional
+	public Laboratorio insert(Laboratorio obj) {
+		obj.setId(null);
+		return repository.save(obj);
+	}
+
+	public Laboratorio update(Laboratorio obj) {
+		findById(obj.getId());
+		return repository.save(obj);
+	}
+
+	public void delete(Integer id) {
+		findById(id);
+		repository.deleteById(id);
+	}
+
+	public List<Laboratorio> findAll() {
+		return repository.findAll();
+	}
+
+	public Page<Laboratorio> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage,Direction.valueOf(direction), orderBy);
+		return repository.findAll(pageRequest);
+	}} 
