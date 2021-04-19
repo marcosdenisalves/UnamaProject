aa077fe6df7dca2b5f5eb7dcc8107be1e71598f3
Study Profile <dee.alves2@gmail.com>
2021-04-19 20:47:49 -0300

    Expondo o Header Authorization(Problemas no Cors)

--- src/main/java/br/com/unamaproject/server/config/SecurityConfig.java
+++ src/main/java/br/com/unamaproject/server/config/SecurityConfig.java
@@ -46,7 +46,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	};
 	
 	private static final String[] PUBLIC_MATCHERS_POST = {
-			"/usuarios/**",
+			"/usuarios",
 			"/auth/forgot/**"
 	};
 	
