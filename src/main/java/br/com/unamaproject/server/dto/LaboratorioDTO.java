6a7eac7423bf4350d47ea4d348649c90bc3ff567
Study Profile <dee.alves2@gmail.com>
2021-05-25 00:59:43 -0300

    Criando laboratorioDTO

---
+++ src/main/java/br/com/unamaproject/server/dto/LaboratorioDTO.java
@@ -0,0 +1,60 @@
+package br.com.unamaproject.server.dto;
+
+import java.io.Serializable;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+public class LaboratorioDTO implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@NotNull
+	private Integer id;
+	
+	@NotEmpty
+	private String nome;
+	
+	@NotEmpty
+	private String descricao;
+	
+	@NotEmpty
+	private String imgUrl;
+
+	public LaboratorioDTO(String nome, String imgUrl, String descricao) {
+		this.nome = nome;
+		this.imgUrl = imgUrl;
+		this.descricao = descricao;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
+	}
+}
