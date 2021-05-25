11c664b4e82372a543f33a78015ae954af459989
Study Profile <dee.alves2@gmail.com>
2021-05-25 00:53:32 -0300

    Refatorando dbService

--- src/main/java/br/com/unamaproject/server/service/DBService.java
+++ src/main/java/br/com/unamaproject/server/service/DBService.java
@@ -34,43 +34,38 @@ public class DBService {
 	public void instantiateTestDatabase() {
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 		
-		Laboratorio lab1 = new Laboratorio(null, "Informática", "Laborátorio de Informática da Unama");
-		Laboratorio lab2 = new Laboratorio(null, "Química", "Laborátorio de Química da Unama");
-		Laboratorio lab3 = new Laboratorio(null, "Gastronomia", "Laborátorio de Gastronomia da Unama");
+		Laboratorio lab1 = new Laboratorio(null, "Biblioteca", "img/biblioteca.png","Biblioteca da Unama");
+		Laboratorio lab2 = new Laboratorio(null, "Química", "img/lab-quimica.png", "Laborátorio de Química");
+		Laboratorio lab3 = new Laboratorio(null, "Gastronomia", "img/lab-106.png", "Laborátorio de Gastronomia");
+		Laboratorio lab4 = new Laboratorio(null, "Engenharia Civil", "img/lab-egn-civil.png", "Laborátorio de Engenharia Civil");
+		Laboratorio lab5 = new Laboratorio(null, "Moda", "img/lab-moda.png", "Laborátorio de Moda");
+		Laboratorio lab6 = new Laboratorio(null, "Cam", "img/lacam.png", "Laborátorio de cam");
+		Laboratorio lab7 = new Laboratorio(null, "102", "img/lab-102.png", "Laborátorio de 102");
 
 		Usuario user1 = new Usuario(null, "Daywison", "da Silva de Souza", "daywison.silva@gmail.com", encoder.encode("1234"));
 		Usuario user2 = new Usuario(null, "Paulo Roberto", "de Souza Soares", "pauloroberto.souza@yahoo.com.br", encoder.encode("4444"));
 		Usuario user3 = new Usuario(null, "Thiago", "de Oliveira Gomes", "thiagodeoliveira@hotmail.com", encoder.encode("2222"));
-		Usuario user4 = new Usuario(null, "Marcos Dênis", "Alves Lucio Junior", "marcos.denis@hotmail.com", encoder.encode("4321"));
-		Usuario user5 = new Usuario(null, "Carlos André", "Pereira Alves", "carlos.alves@hotmail.com", encoder.encode("55555"));
-		Usuario user6 = new Usuario(null, "Fulano", "Sicrano de tal", "fulano.sicrano@yahoo.com.br", encoder.encode("3333"));
 		Usuario adm = new Usuario(null, "Administrador", "Adm", "administrador@adm.com", encoder.encode("adm"));
 		
 		Avaliacao av1 = new Avaliacao(null, 2, "Não gostei do conteudo", LocalDate.parse("13/11/2019", formatter).atStartOfDay(), user1, lab1);
-		Avaliacao av2 = new Avaliacao(null, 5, "Excelente projeto", LocalDate.parse("15/09/2020", formatter).atStartOfDay(), user1, lab1);
-		Avaliacao av3 = new Avaliacao(null, 3, "Acredito que o conteúdo aprensentado poderia ser melhor", LocalDate.parse("08/02/2021",formatter).atStartOfDay(), user1, lab1);
-		
-		Avaliacao av4 = new Avaliacao(null, 5, "Excelente conteudo", LocalDate.parse("01/05/2015", formatter).atStartOfDay(), user2, lab2);
-		Avaliacao av5 = new Avaliacao(null, 4, "Otimo trabalho galera", LocalDate.parse("02/07/2016", formatter).atStartOfDay(), user2, lab2);
-		Avaliacao av6 = new Avaliacao(null, 3, "Precisa melhorar", LocalDate.parse("13/12/2021", formatter).atStartOfDay(), user2, lab2);
-		
-		Avaliacao av7 = new Avaliacao(null, 2, "Não ficou muito bom", LocalDate.parse("05/01/2020", formatter).atStartOfDay(), user3, lab3);
-		Avaliacao av8 = new Avaliacao(null, 5, "Excelente desenvolvimento e conclusão", LocalDate.parse("04/02/2018", formatter).atStartOfDay(), user3, lab3);
-		Avaliacao av9 = new Avaliacao(null, 3, "Boa galera, gostei demais!", LocalDate.parse("01/10/2021", formatter).atStartOfDay(), user3, lab3);
-		
+		Avaliacao av2 = new Avaliacao(null, 5, "Excelente conteudo", LocalDate.parse("01/05/2015", formatter).atStartOfDay(), user2, lab2);
+		Avaliacao av3 = new Avaliacao(null, 3, "Boa galera, gostei demais!", LocalDate.parse("01/10/2021", formatter).atStartOfDay(), user3, lab3);
+				
 		adm.addPerfil(PerfilAcesso.ADMIN);
 		user1.addPerfil(PerfilAcesso.USUARIO);
+		user2.addPerfil(PerfilAcesso.USUARIO);
+		user3.addPerfil(PerfilAcesso.USUARIO);
 		
-		user1.getAvaliacoes().addAll(Arrays.asList(av1, av2, av3));
-		user2.getAvaliacoes().addAll(Arrays.asList(av4, av5, av6));
-		user3.getAvaliacoes().addAll(Arrays.asList(av7, av8, av9));
+		user1.getAvaliacoes().addAll(Arrays.asList(av1));
+		user2.getAvaliacoes().addAll(Arrays.asList(av2));
+		user3.getAvaliacoes().addAll(Arrays.asList(av3));
 		
 		lab1.getAvaliacoes().addAll(Arrays.asList(av1, av2, av3));
-		lab2.getAvaliacoes().addAll(Arrays.asList(av4, av5, av6));
-		lab3.getAvaliacoes().addAll(Arrays.asList(av7, av8, av9));
+		lab2.getAvaliacoes().addAll(Arrays.asList(av1, av2, av3));
+		lab3.getAvaliacoes().addAll(Arrays.asList(av1, av2, av3));
 		
-		laboratorioRepository.saveAll(Arrays.asList(lab1, lab2, lab3));
-		usuarioRepository.saveAll(Arrays.asList(user1, user2, user3, user4, user5, user6, adm));
-		avaliacaoRepository.saveAll(Arrays.asList(av1, av2, av3, av4, av5, av6, av7, av8, av9));
+		laboratorioRepository.saveAll(Arrays.asList(lab1, lab2, lab3, lab4, lab5, lab6, lab7));
+		usuarioRepository.saveAll(Arrays.asList(adm, user1, user2, user3));
+		avaliacaoRepository.saveAll(Arrays.asList(av1, av2, av3));
 	}
 }
